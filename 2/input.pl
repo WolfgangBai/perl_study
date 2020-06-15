b98163ff2293eb5e3925c9318e51c29aa756ad93
chenggang7 <chenggang7@staff.sina.com.cn>
2020-06-15 19:44:09 +0800

    chapter 2

---
+++ 2/input.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+$line = <STDIN>;
+if ($line eq "\n") {
+    print "That was just a blank line!\n"
+} {
+    print "value:$line\n"
+}
---
+++ 2/var.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+
+$line = "line";
+if ($line eq "ine") {
+    print "equal\n"
+} else {
+    print "not equal\n"
+}
